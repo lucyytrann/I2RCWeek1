5c8fb444682535ca1e63c886d90b2b04b44c04b1
Lucy Tran <nhitran122007@gmail.com>
2023-09-29 13:59:02 -0700

    Comments on DriveTrain

--- src/main/java/frc/robot/subsystems/DriveTrain.java
+++ src/main/java/frc/robot/subsystems/DriveTrain.java
@@ -22,6 +22,7 @@ import com.kauailabs.navx.frc.AHRS;
 
 public class DriveTrain extends SubsystemBase 
 {
+  //Decalre and initialize the variale 
   private final WPI_TalonSRX leftDriveTalon;
   private final WPI_TalonSRX rightDriveTalon;
 
@@ -34,47 +35,60 @@ public class DriveTrain extends SubsystemBase
   /** Creates a new DriveTrain */
   public DriveTrain() 
   {
+    //Intialized leftDriveTalon and rightDriveTalon
     leftDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.LeftDriveTalonPort);
     rightDriveTalon = new WPI_TalonSRX(Constants.DriveTrainPorts.RightDriveTalonPort);
-  
+    
+    //Set neutral mode to both leftDriveTalon and right DriveTalon
     leftDriveTalon.setNeutralMode(NeutralMode.Coast);
     rightDriveTalon.setNeutralMode(NeutralMode.Coast);
 
+    //Set true to inverted of leftDriveTalon, set false to inverted of rightDriveTalan
     leftDriveTalon.setInverted(true);
     rightDriveTalon.setInverted(false);
 
+    //Set true to sensor phase of both leftDriveTalon and rightDriveTalon
     leftDriveTalon.setSensorPhase(true);
     rightDriveTalon.setSensorPhase(true);
-
+    
+    //Config the Factory Defalt of the leftDriveTalon
     leftDriveTalon.configFactoryDefault();
+    //Config the selected feedback sensor of the leftDriveTalon
     leftDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
+    //Config the factory defalt of the rightDriveTalon
     rightDriveTalon.configFactoryDefault();
+    //Config the selected feedback sensor of the rightDriveTalon
     rightDriveTalon.configSelectedFeedbackSensor(FeedbackDevice.CTRE_MagEncoder_Relative, 0, 10);
 
   }
-
+  //Set the rightSpeed of the EightDriveTalon and set the leftSpeed of the leftDriveTalon
   public void tankDrive(double leftSpeed, double rightSpeed) {
     rightDriveTalon.set(rightSpeed);
     leftDriveTalon.set(leftSpeed);
   }
 
+  //Reset tthe encoders 
   public void resetEncoders() {
     leftDriveTalon.setSelectedSensorPosition(0,0,10);
     rightDriveTalon.setSelectedSensorPosition(0,0,10);
   }
 
+  //Return the TIcks by calculated the average of selected sensor position of leftDriveTalon and rightDriveTalon
   public double getTicks() {
     return (leftDriveTalon.getSelectedSensorPosition(0) + rightDriveTalon.getSelectedSensorPosition(0)) / 2.0;
   }
  
+  //Get angle of navx
   public double getAngle(){
     return navx.getAngle(); 
   }
  
+  //Reset the navx
   public void resetNavx(){
     navx.reset();
   }
 
+  //Override the periodic by using SmartDashboard value 
   @Override
   public void periodic() {
     SmartDashboard.putNumber("Left Voltage", leftDriveTalon.getMotorOutputPercent());
